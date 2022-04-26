0301916e5c79d94266bffddc6000f7236d8f5c1b
HridumD <98991490+HridumD@users.noreply.github.com>
2022-04-25 20:05:05 -0400

    Romance generator complete
    Finished the romance generator, with character, and plot generation. May change some things later, and will fix bugs.

--- wpg/romance/logic.js
+++ wpg/romance/logic.js
@@ -24,10 +24,31 @@ function generateStory(){
         'has lost their talent',
         'is financially unstable'
     ]
+    var types = [
+        'romantic comedy',
+        'romantic suspense',
+        'romantic mystery',
+        'historical romance',
+        'inspirational romance'
+    ]
+    places = [
+        'a beach',
+        'a grocery store',
+        'a highschool',
+        'a gas station'
+    ]
+    reasons = [
+        "someone didn't have enough change",
+        "someone's close relative died",
+        'someone was trying to buy food'
+    ]
 
     var l1 = 'A ' + profs[Math.floor(Math.random() * profs.length)] + ' who ' + adjs[Math.floor(Math.random() * adjs.length)]
     var l2 = 'A ' + profs[Math.floor(Math.random() * profs.length)] + ' who ' + adjs[Math.floor(Math.random() * adjs.length)]
 
+    var plot = 'A ' + types[Math.floor(Math.random() * types.length)] + ', where the plot starts off at ' + places[Math.floor(Math.random() * places.length)] + '. It starts when ' + reasons[Math.floor(Math.random() * reasons.length)] 
+
+    document.getElementById('pd').innerHTML = plot
     document.getElementById('l1d').innerHTML = l1
     document.getElementById('l2d').innerHTML = l2
 }
